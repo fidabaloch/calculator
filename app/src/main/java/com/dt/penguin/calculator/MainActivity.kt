ea41ef4bb515b02cb8dc5f0bc0a1d88a0a27ac8a
penguin <fida_baloch@hotmail.com>
2018-12-29 03:33:33 +0500

    Compile time Error Resolved

--- app/src/main/java/com/dt/penguin/calculator/MainActivity.kt
+++ app/src/main/java/com/dt/penguin/calculator/MainActivity.kt
@@ -2,11 +2,58 @@ package com.dt.penguin.calculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun NumClickEvent (view: View){
+        var displayScreen : String = etDisplay.text.toString()
+        var btnPressed: Button = view as Button
+        when(btnPressed){
+            btn0-> {
+                displayScreen+="0"
+            }
+            btn1-> {
+                displayScreen+="1"
+            }
+            btn2-> {
+                displayScreen+="2"
+            }
+            btn3-> {
+                displayScreen+="3"
+            }
+            btn4-> {
+                displayScreen+="4"
+            }
+            btn5-> {
+                displayScreen+="5"
+            }
+            btn6-> {
+                displayScreen+="6"
+            }
+            btn7-> {
+                displayScreen+="7"
+            }
+            btn8-> {
+                displayScreen+="8"
+            }
+            btn9-> {
+                displayScreen+="9"
+            }
+            btnDot-> {
+                //TODO dot shall be pressed once
+                displayScreen+="."
+            }
+            btnPlusMinus-> {
+                displayScreen="-"+displayScreen
+            }
+        }
+        etDisplay.setText(displayScreen)
     }
 }
