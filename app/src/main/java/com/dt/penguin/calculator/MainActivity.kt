4b1276449a6887e197b7ead5a4c5d1381e850bb7
Fida Ahmed <fida_baloch@hotmail.com>
2018-12-29 18:53:37 +0500

    all funcationality completed

--- app/src/main/java/com/dt/penguin/calculator/MainActivity.kt
+++ app/src/main/java/com/dt/penguin/calculator/MainActivity.kt
@@ -7,12 +7,20 @@ import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    var opSelected:String = ""
+    var isNewOp:Boolean = true
+    var firstNumber:String=""
+    var secondNumber:String=""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
     fun NumClickEvent (view: View){
+
+        if(isNewOp){
+            etDisplay.setText("")
+        }
+        isNewOp=false
         var displayScreen : String = etDisplay.text.toString()
         var btnPressed: Button = view as Button
         when(btnPressed){
@@ -55,5 +63,54 @@ class MainActivity : AppCompatActivity() {
             }
         }
         etDisplay.setText(displayScreen)
+    }
+    fun OperationEvent(view:View){
+        val opBtn: Button = view as Button
+        when(opBtn){
+            btnAddition ->{
+                opSelected="+"
+            }
+            BtnSubtract ->{
+                opSelected="-"
+            }
+            btnMultiply->{
+                opSelected="*"
+            }
+            btnDivide ->{
+                opSelected="/"
+            }
+        }
+        firstNumber=etDisplay.text.toString()
+        isNewOp=true
+    }
+    fun EqualEvent(view:View){
+        secondNumber=etDisplay.text.toString()
+        var result:Double?=null
+        when(opSelected){
+            "+"->{
+                result = firstNumber.toDouble() + secondNumber.toDouble()
+            }
+            "-"->{
+                result = firstNumber.toDouble() - secondNumber.toDouble()
+            }
+            "*"->{
+                result = firstNumber.toDouble() * secondNumber.toDouble()
+            }
+            "/"->{
+                result = firstNumber.toDouble() / secondNumber.toDouble()
+            }
+        }
+        etDisplay.setText(result.toString())
+        isNewOp=true
+    }
+    fun CleanEvent(view:View){
+        etDisplay.setText("")
+        isNewOp=true
+    }
+    fun PercentageEvent(view:View){
+        var number:Double=etDisplay.text.toString().toDouble()
+        number/=100
+        etDisplay.setText(number.toString())
+        isNewOp=true
     }
 }
